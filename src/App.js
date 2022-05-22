d581e0238d6a83c1ccd91c314f40217f7687db0c
troy <tmatthews@coreblue.co.uk>
2022-05-22 16:00:52 +0100

    changes to Navbar

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ function App() {
   return (
     <div className="bg-neutral-800 h-screen ">
       <Navbar/>
-      <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
     </div>
   );
 }
---
+++ src/components/Nav_Item.js
@@ -0,0 +1,6 @@
+
+
+export default function NavItem(props) { 
+  return <a href="#" className={props.classes} aria-current="page">{props.children}</a>
+
+}
