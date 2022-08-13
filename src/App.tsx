9aef92bc68128f2c9841adb3ab6fba0f7998612d
Troy <TroyMatthewsHong@gmail.com>
2022-08-13 23:12:27 +0100

    add navbar and display center text on screens larger than small

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="bg-neutral-800 min-h-screen">
+      <Navbar/>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -17,8 +16,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+      
     </div>
   );
 }
